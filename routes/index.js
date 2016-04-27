e943844771a599b31d32b9678c3858c9f661d2ce
Kishor Rana <3philic@users.noreply.github.com>
2016-04-27 15:49:03 +0200

    Starting routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var options = {
+         title : 'Geo Location App'
+    };
+    res.render('index', options);
+});
+/* GET tagging page. */
+router.get('/tagging', function(req, res, next) {
+    var options = {
+         title : 'Geo Location Tagging'
+    };
+    res.render('tagging', options);
+});
+/* GET discovery page. */
+router.get('/discovery', function(req, res, next) {
+    var options = {
+         title : 'Geo Location Discovery',
+         locations : [ {
+             lat : 49.013790,
+             long : 8.404435,
+             name : 'castle',
+             hash : '#sight'
+         }, {
+             lat : 49.013790,
+             long : 8.390071,
+             name : 'iwi',
+             hash : '#edu'
+}] };
+    res.render('discovery', options);
 });
 
 module.exports = router;
