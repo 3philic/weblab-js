6470088aedd259c4dc2a62ddd3d8412b04d9f74e
Max Baumbach <bearjaw@users.noreply.github.com>
2016-05-13 15:57:04 +0200

    added save method, use redis-commander to inspect database!

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,70 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+var client = redis.createClient();
 var locs;
-
 var locationRepository = require('./locationRepository');
+
 
 // Middlewares
 function showTagging(req, res) {
-  var options = {
-       title : 'Geo Location Tagging'
-  };
-  res.render('tagging', options);
+    var options = {
+        title: 'Geo Location Tagging'
+    };
+    res.render('tagging', options);
 }
 
 var createDemoLocationsDoOnce = false;
+
 function createDemoLocations(req, res, next) {
-  if (!createDemoLocationsDoOnce) {
-    locationRepository.createLocation(49.013790, 8.404435, 'castle', '#sight');
-    locationRepository.createLocation(49.013790, 8.390071, 'iwi', '#edu');
-    createDemoLocationsDoOnce = true;
-  }
-  next();
+    if (!createDemoLocationsDoOnce) {
+        locationRepository.createLocation(49.013790, 8.404435, 'castle', '#sight');
+        locationRepository.createLocation(49.013790, 8.390071, 'iwi', '#edu');
+        createDemoLocationsDoOnce = true;
+    }
+    next();
 }
 
 function getLocations(req, res, next) {
-  var searchName = req.query.name;
+    var searchName = req.query.name;
 
-  if (!searchName) {
-    searchName = "";
-  }
+    if (!searchName) {
+        searchName = "";
+    }
 
-  var allLocations = locationRepository.getLocations();
-  var relevantLocations = [];
+    var allLocations = locationRepository.getLocations();
+    var relevantLocations = [];
 
-  // aquire locations with matching names to search term
-  for (var i = 0; i < allLocations.length; i++) {
-     if (allLocations[i].name.toLowerCase().indexOf(searchName) > -1) {
-         relevantLocations.push(allLocations[i]);
-       }
-  }
-  // aquire locations with matching hashtags to search term
-  for (var i = 0; i < allLocations.length; i++) {
-     if (allLocations[i].hash.toLowerCase().indexOf(searchName) > -1) {
-         // check if the location has already been added to the relevantLocations
-         var relevantLocationsContainsLocation = false;
-         for (var j = 0; j < relevantLocations.length; j++) {
-           if (allLocations[i].id == relevantLocations[j].id) {
-             relevantLocationsContainsLocation = true;
-           }
-         }
-         if (!relevantLocationsContainsLocation) {
-           relevantLocations.push(allLocations[i]);
-         }
-       }
-  }
+    // aquire locations with matching names to search term
+    for (var i = 0; i < allLocations.length; i++) {
+        if (allLocations[i].name.toLowerCase().indexOf(searchName) > -1) {
+            relevantLocations.push(allLocations[i]);
+        }
+    }
+    // aquire locations with matching hashtags to search term
+    for (var i = 0; i < allLocations.length; i++) {
+        if (allLocations[i].hash.toLowerCase().indexOf(searchName) > -1) {
+            // check if the location has already been added to the relevantLocations
+            var relevantLocationsContainsLocation = false;
+            for (var j = 0; j < relevantLocations.length; j++) {
+                if (allLocations[i].id == relevantLocations[j].id) {
+                    relevantLocationsContainsLocation = true;
+                }
+            }
+            if (!relevantLocationsContainsLocation) {
+                relevantLocations.push(allLocations[i]);
+            }
+        }
+    }
 
-  req.locations = relevantLocations;
-  next();
+    req.locations = relevantLocations;
+    next();
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var options = {
-         title : 'Geo Location App'
+        title: 'Geo Location App'
     };
     res.render('index', options);
 });
@@ -69,20 +72,52 @@ router.get('/', function(req, res, next) {
 router.get('/tagging', showTagging);
 /* POST tagging page. */
 router.post('/tagging', function(req, res, next) {
-  console.log(req.body);
-  locationRepository.createLocation(req.body['latitude'], req.body['longitude'],
-    req.body['name'], req.body['hashtag']);
+    console.log(req.body);
+    locationRepository.createLocation(req.body['latitude'], req.body['longitude'],
+        req.body['name'], req.body['hashtag']);
 
-  next();
+    next();
 }, showTagging);
 /* GET discovery page. */
 router.get('/discovery', createDemoLocations, getLocations, function(req, res, next) {
 
-  var options = {
-        title : 'Geo Location Discovery',
-        locations : req.locations };
+    var options = {
+        title: 'Geo Location Discovery',
+        locations: req.locations
+    };
     locs = options[2];
     res.render('discovery', options);
 });
+
+
+//REDIS
+
+client.on('connect', function(){
+  console.log('connected');
+});
+
+exports.save = function(key, value, callback) {
+  console.log("function save has been called");
+    function saveIfNew(err, exists) {
+        if (!err && !exists) {
+            client.multi().set(key, JSON.stringify(value)).zadd("index", "1", key).exec(function(err, results) {
+                    callback(err, results)
+                    console.log("okay");
+                });
+        } else
+            callback(err, exists);
+    }
+    client.exists(key, saveIfNew);
+};
+
+function get(key, callback) {
+    client.get(key, function(err, res) {
+        if (err)
+            callback(err, null);
+        else
+        callback(null, JSON.parse(res));
+    });
+};
+
 
 module.exports = router;
--- routes/locationRepository.js
+++ routes/locationRepository.js
@@ -1,19 +1,24 @@
 var locations = []; // saves the locations in memory
 var nextNewId = 0;
+var route = require('./index');
 
 exports.createLocation = function(lat, long, name, hash) {
-  var location = {
-            lat : lat,
-            long : long,
-            name : name,
-            hash : hash,
-            id : nextNewId};
-  locations.push(location);
+    var location = {
+        lat: lat,
+        long: long,
+        name: name,
+        hash: hash,
+        id: nextNewId
+    };
+    route.save(nextNewId, location, function(saveErr, results) {
+        console.log("yo");
+    });
+    locations.push(location);
 
-  nextNewId++;
-  return location;
+    nextNewId++;
+    return location;
 }
 
 exports.getLocations = function() {
-  return locations;
+    return locations;
 }
